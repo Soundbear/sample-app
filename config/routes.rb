1c983480a3a7bff18a0dc7bf42fdff034fa680f7
Addison Jones <addison.jones3@gmail.com>
2018-03-28 21:27:24 +0000

    End of Exercise 3.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  
+  get 'foo/bar'
+
+  get 'foo/baz'
+
+  root 'application#hello'
   get 'static_pages/home'
 
   get 'static_pages/help'
 
-  root 'application#hello'
 end
